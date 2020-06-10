6c6c6e7625075ab0aca6b7ec8fc812160a9e2011
TomasBraga <tbragaino@gmail.com>
2020-06-10 15:18:40 +0100

    a

--- register.php
+++ register.php
@@ -4,33 +4,33 @@
 	$DATABASE_USER = 'root';
 	$DATABASE_PASS = '';
 	$DATABASE_NAME = 'pw2';
-	// Try and connect using the info above.
+	//Tenta estabelecer ligação à base de dados
 	$con = mysqli_connect($DATABASE_HOST, $DATABASE_USER, $DATABASE_PASS, $DATABASE_NAME);
 	if (mysqli_connect_errno())
 	{
 		//Se houver algum erro/problema com a conexão, para o script e apresenta o erro
 		exit('Conexão falhada na conexão com o SQL: ' . mysqli_connect_error());
 	}
-	// Now we check if the data was submitted, isset() function will check if the data exists.
+	//Verificamos se os dados existem
 	if (!isset($_POST['username'], $_POST['password'], $_POST['email']))
 	{
-		// Could not get the data that should have been sent.
+		//Não foram obtidos os dados que deveriam ter sido enviados
 		exit('Por favor, preencha todos os campos do formulário');
 	}
-	// Make sure the submitted registration values are not empty.
+	//Verifica se os campos de registo enviados não estão vazios
 	if (empty($_POST['username']) || empty($_POST['password']) || empty($_POST['email']))
 	{
 		//Um ou mais valores estão vazios
 		exit('Por favor, preencha todos os campos do formulário');
 	}
-	// We need to check if the account with that username exists.
+	//Verifica se existe alguma conta com o mesmo nome de utilizador
 	if ($stmt = $con->prepare('SELECT id, password FROM accounts WHERE username = ?'))
 	{
-		// Bind parameters (s = string, i = int, b = blob, etc), hash the password using the PHP password_hash function.
+		//Parâmetros de ligação e hash da senha usando a função PHP passoword_hash
 		$stmt->bind_param('s', $_POST['username']);
 		$stmt->execute();
 		$stmt->store_result();
-		// Store the result so we can check if the account exists in the database.
+		//Armazena o resultado para que se possa verificar se a conta existe na base de dados
 		if ($stmt->num_rows > 0)
 		{
 			//Utilizador já existente
@@ -53,7 +53,7 @@
 				{
 					exit('A palavra-passe deve ter entre 5 e 20 carateres');
 				}
-				// We do not want to expose passwords in our database, so hash the password and use password_verify when a user logs in.
+				//Para não se expor a palavra-passe na base de dados, realiza-se o hash da senha e usa o password_verify quando o utilizador efetuar o login
 				$password = password_hash($_POST['password'], PASSWORD_DEFAULT);
 				$stmt->bind_param('sss', $_POST['username'], $password, $_POST['email']);
 				$stmt->execute();
@@ -61,16 +61,16 @@
 			}
 			else
 			{
-				// Something is wrong with the sql statement, check to make sure accounts table exists with all 3 fields.
-				echo 'Could not prepare statement!';
+				//Houve um erro na instrução SQL, verifique se a tabela existe com os 3 campos
+				echo 'Ocorreu um erro';
 			}
 		}
 		$stmt->close();
 	}
 	else
 	{
-		// Something is wrong with the sql statement, check to make sure accounts table exists with all 3 fields.
-		echo 'Could not prepare statement!';
+		//Houve um erro na instrução SQL, verifique se a tabela existe com os 3 campos
+		echo 'Ocorreu um erro';
 	}
 	$con->close();
 ?>
