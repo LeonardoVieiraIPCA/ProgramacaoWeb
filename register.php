f4d0da45c05116234298e15a9038decac50c33aa
TomasBraga <tbragaino@gmail.com>
2020-06-10 14:53:17 +0100

    a

--- register.php
+++ register.php
@@ -1,5 +1,5 @@
 <?php
-	// Change this to your connection info.
+	//Informações relativas à conexão com a base de dados
 	$DATABASE_HOST = 'localhost';
 	$DATABASE_USER = 'root';
 	$DATABASE_PASS = '';
@@ -8,7 +8,7 @@
 	$con = mysqli_connect($DATABASE_HOST, $DATABASE_USER, $DATABASE_PASS, $DATABASE_NAME);
 	if (mysqli_connect_errno())
 	{
-		// If there is an error with the connection, stop the script and display the error.
+		//Se houver algum erro/problema com a conexão, para o script e apresenta o erro
 		exit('Conexão falhada na conexão com o SQL: ' . mysqli_connect_error());
 	}
 	// Now we check if the data was submitted, isset() function will check if the data exists.
