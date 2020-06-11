77e554aca701fef9e111f567ba79d05721c93d45
TomasBraga <tbragaino@gmail.com>
2020-06-11 14:30:17 +0100

    a

--- authenticate.php
+++ authenticate.php
@@ -1,48 +1,62 @@
 <?php
-session_start();
-// Change this to your connection info.
-$DATABASE_HOST = 'localhost';
-$DATABASE_USER = 'root';
-$DATABASE_PASS = '';
-$DATABASE_NAME = 'pw2';
-// Try and connect using the info above.
-$con = mysqli_connect($DATABASE_HOST, $DATABASE_USER, $DATABASE_PASS, $DATABASE_NAME);
-if ( mysqli_connect_errno() ) {
-	// If there is an error with the connection, stop the script and display the error.
-	exit('Failed to connect to MySQL: ' . mysqli_connect_error());
-}
-// Now we check if the data from the login form was submitted, isset() will check if the data exists.
-if (!isset($_POST['username'], $_POST['password'])) {
-	// Could not get the data that should have been sent.
-	exit('Please fill both the username and password fields!');
-}
-// Prepare our SQL, preparing the SQL statement will prevent SQL injection.
-if ($stmt = $con->prepare('SELECT id, password FROM accounts WHERE username = ?')) {
-	// Bind parameters (s = string, i = int, b = blob, etc), in our case the username is a string so we use "s"
-	$stmt->bind_param('s', $_POST['username']);
-	$stmt->execute();
-	// Store the result so we can check if the account exists in the database.
-	$stmt->store_result();
-
-	if ($stmt->num_rows > 0) {
-		$stmt->bind_result($id, $password);
-		$stmt->fetch();
-		// Account exists, now we verify the password.
-		// Note: remember to use password_hash in your registration file to store the hashed passwords.
-		if (password_verify($_POST['password'], $password)) {
-			// Verification success! User has loggedin!
-			// Create sessions so we know the user is logged in, they basically act like cookies but remember the data on the server.
-			session_regenerate_id();
-			$_SESSION['loggedin'] = TRUE;
-			$_SESSION['name'] = $_POST['username'];
-			$_SESSION['id'] = $id;
-			header('Location: home.php');
-		} else {
-			echo 'Incorrect password!';
-		}
-	} else {
-		echo 'Incorrect username!';
+	session_start();
+	//Informações relativas à conexão com a base de dados
+	$DATABASE_HOST = 'localhost';
+	$DATABASE_USER = 'root';
+	$DATABASE_PASS = '';
+	$DATABASE_NAME = 'pw2';
+	//Tenta estabelecer ligação à base de dados
+	$con = mysqli_connect($DATABASE_HOST, $DATABASE_USER, $DATABASE_PASS, $DATABASE_NAME);
+	if ( mysqli_connect_errno())
+	{
+		//Se houver algum erro/problema com a conexão, para o script e apresenta o erro
+		exit('Conexão falhada na conexão com o SQL: ' . mysqli_connect_error());
 	}
-
-	$stmt->close();
-}
+	//Verificamos se os dados do formulário de login foram enviados, a função isset() verifica se os dados existem
+	if (!isset($_POST['username'], $_POST['password']))
+	{
+		//Não foi possível obter os dados que deviam ter sido enviados
+		exit('Por favor, preencha os campos do nome de utilizador e a palavra-passe');
+	}
+	//Prepara a instrução SQL e impedirá a injeção de SQL
+	if ($stmt = $con->prepare('SELECT id, password FROM accounts WHERE username = ?'))
+	{
+		//Parâmetros de ligação
+		$stmt->bind_param('s', $_POST['username']);
+		$stmt->execute();
+		//Armazena o resultado para que se possa verificar se a conta existe na base de dados
+		$stmt->store_result();
+		if ($stmt->num_rows > 0)
+		{
+			$stmt->bind_result($id, $password);
+			$stmt->fetch();
+			//A conta existe, vericação da senha
+			if (password_verify($_POST['password'], $password))
+			{
+				//O login foi realizado com sucesso
+				//Cria uma sessão para que saiba que o utilizador está com o login efetuado com sucesso. Este código age como um cookie, lembrando o servidor dos dados
+				session_regenerate_id();
+				$_SESSION['loggedin'] = TRUE;
+				$_SESSION['name'] = $_POST['username'];
+				$_SESSION['id'] = $id;
+				//Configuração dos cookies
+				//Este cookie vai expirar 30 dias depois de ter sido criado
+				//O "/" significa que este cookie vai estar disponível em todo o site
+				setcookie("username", "", time()+(86400 * 30), "/");
+				header('Location: home.php');
+				echo "Bem vindo" .$_SESSION["name"];
+			}
+			else
+			{
+				echo 'Palavra-passe incorreta';
+			}
+		}
+		else
+		{
+			echo 'Nome de utilizador incorreto';
+		}
+		$stmt->close();
+		//Remove todos os dados da sessão
+		session_destroy();
+	}
+?>
