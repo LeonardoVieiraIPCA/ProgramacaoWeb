c33e815e80270bd6e63374342a67022346e28c74
TomasBraga <tbragaino@gmail.com>
2020-06-11 14:37:15 +0100

    a

--- profile.php
+++ profile.php
@@ -1,27 +1,30 @@
 <?php
-// We need to use sessions, so you should always start sessions using the below code.
-session_start();
-// If the user is not logged in redirect to the login page...
-if (!isset($_SESSION['loggedin'])) {
-	header('Location: index.html');
-	exit;
-}
-$DATABASE_HOST = 'localhost';
-$DATABASE_USER = 'root';
-$DATABASE_PASS = '';
-$DATABASE_NAME = 'pw2';
-$con = mysqli_connect($DATABASE_HOST, $DATABASE_USER, $DATABASE_PASS, $DATABASE_NAME);
-if (mysqli_connect_errno()) {
-	exit('Failed to connect to MySQL: ' . mysqli_connect_error());
-}
-// We don't have the password or email info stored in sessions so instead we can get the results from the database.
-$stmt = $con->prepare('SELECT password, email FROM accounts WHERE id = ?');
-// In this case we can use the account ID to get the account info.
-$stmt->bind_param('i', $_SESSION['id']);
-$stmt->execute();
-$stmt->bind_result($password, $email);
-$stmt->fetch();
-$stmt->close();
+	//Inicia uma sessão
+	session_start();
+	//Caso o utilizador não estiver com o login realizado, é encaminhado para a página de login
+	if (!isset($_SESSION['loggedin']))
+	{
+		header('Location: index.html');
+		exit;
+	}
+	//Informações relativas à base de dados
+	$DATABASE_HOST = 'localhost';
+	$DATABASE_USER = 'root';
+	$DATABASE_PASS = '';
+	$DATABASE_NAME = 'pw2';
+	$con = mysqli_connect($DATABASE_HOST, $DATABASE_USER, $DATABASE_PASS, $DATABASE_NAME);
+	if (mysqli_connect_errno())
+	{
+		exit('Erro na conexão com o SQL: ' . mysqli_connect_error());
+	}
+	//Como não temos as informações da palabra-passe e do email armazenadas nas sessões, temos de as ir buscar/procurar à base de dados
+	$stmt = $con->prepare('SELECT password, email FROM accounts WHERE id = ?');
+	//Neste caso, podemos usar o ID da conta para se obter as informações relativas à conta do utilizador
+	$stmt->bind_param('i', $_SESSION['id']);
+	$stmt->execute();
+	$stmt->bind_result($password, $email);
+	$stmt->fetch();
+	$stmt->close();
 ?>
 <!DOCTYPE html>
 <html>
