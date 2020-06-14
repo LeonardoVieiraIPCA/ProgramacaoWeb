e9d33c32afad9984d8c2fa6897f854c11ffebb5e
TomasBraga <tbragaino@gmail.com>
2020-06-14 15:35:07 +0100

    a

--- profile.php
+++ profile.php
@@ -26,6 +26,35 @@
 	$stmt->fetch();
 	$stmt->close();
 ?>
+<?php
+	//Inicia uma sessão
+	session_start();
+	//Caso o utilizador não estiver com o login realizado, é encaminhado para a página de login
+	if (!isset($_SESSION['loggedin']))
+	{
+		header("Location: index.html");
+		exit;
+	}
+	//Informações relativas à base de dados
+	$DATABASE_HOST = 'localhost';
+	$DATABASE_USER = 'root';
+	$DATABASE_PASS = '';
+	$DATABASE_NAME = 'pw2';
+	$con = mysqli_connect($DATABASE_HOST, $DATABASE_USER, $DATABASE_PASS, $DATABASE_NAME);
+	if (mysqli_connect_errno())
+	{
+		exit("Erro na conexão com o SQL: " . mysqli_connect_error());
+	}
+	//Como não temos as informações da palabra-passe e do email armazenadas nas sessões, temos de as ir buscar/procurar à base de dados
+	$stmt = $con->prepare('SELECT password, email FROM accounts WHERE id = ?');
+	//Neste caso, podemos usar o ID da conta para se obter as informações relativas à conta do utilizador
+	//Utiliza-se o "i" porque o id é um valor inteiro
+	$stmt->bind_param('i', $_SESSION['id']);
+	$stmt->execute();
+	$stmt->bind_result($password, $email);
+	$stmt->fetch();
+	$stmt->close();
+?>
 <!DOCTYPE html>
 <html>
 	<head>
